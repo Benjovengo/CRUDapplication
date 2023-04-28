428664e1847509ac9fddc1ea54c8d050baf01225
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-28 15:38:51 -0300

    add page boilerplate to insert a product
    The page consists of 3 input fields, and the respective labels for the:
    - product name,
    - product description,
    - price.
    
    The product name and description are respectively limited to 200 and 500 chars while the price information is limited to be able to be stored in a Decimal(10,2) data field in the database.
    
    Stylesheet
    - Each element occupies a separate line
    - Uses "display: flex" and "flex-direction: column" to create a vertical layout.
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>CRUD Application</h1>
+      <h1>Insert Product</h1>
+
+      <div className="form__wrapper">
+        <form>
+          <label for="product-name">Product Name:</label>
+          <input type="text" id="product-name" name="product-name" maxLength="200" required/>
+
+          <label for="product-description">Product Description:</label>
+          <textarea id="product-description" name="product-description" maxLength="500" required></textarea>
+
+          <label for="price">Price:</label>
+          <input type="number" id="price" name="price" step="0.01" min="0" max="9999999.99" required/>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
     </div>
   );
 }
