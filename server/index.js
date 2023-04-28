5d4c8ac27dff86a23a5012e9a075565a3bb2ee2f
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-28 14:13:59 -0300

    add product to database
    Setup the connection pool to the database server running in WSL.
    - the port was changed from the default '3306' to '33061' to avoid clashing with other database server that may be running on the local machine.
    
    - Display the result of the database query in the webpage of Nodemon server at localhost:3001.
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express")
 const app = express()
+const mysql = require("mysql")
+
+// call the database
+const db = mysql.createPool({
+  host: "127.0.0.1",
+  port: '33061',
+  user: "root",
+  password: "ultra_secure_password",
+  database: "ChallengeDB",
+})
 
 app.get("/", (req, res) => {
-  res.send("Nodemon configured!")
+  // DB Query
+  const sqlInsertItem = "INSERT INTO store_products (product_name, description, price, quantity) VALUES ('Chanel Bag', 'A very expensive leather bag.', 1250.00, 1);"
+  db.query(sqlInsertItem, (err, result) => {
+    res.send(result)
+  })
 })
 
 app.listen(3001, ()=>{
