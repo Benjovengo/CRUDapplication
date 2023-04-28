cd1feb82deb47bc1db876c012b7b90270af5941d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-28 20:40:13 -0300

    refactor code to fix issues identified by standard command
    JavaScript Standard Style: for more information about the package refer to the GitHub repo at https://github.com/standard/standard
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,34 @@
-import React, { useEffect, useState } from "react"
-import axios from "axios"
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 // Stylesheet
-import "./App.css"
+import './App.css'
 
-function App() {
-
+function App () {
   // Hooks for the product information
-  const [productName, setProductName] = useState("")
-  const [productDescription, setProductDescription] = useState("")
+  const [productName, setProductName] = useState('')
+  const [productDescription, setProductDescription] = useState('')
   const [productPrice, setProductPrice] = useState(0)
   const [productsList, setProductsList] = useState([])
 
   // Fetch the products from the database when the page loads
   useEffect(() => {
     // Address of the backend server
-    const baseURL = "http://localhost:3001"
-    const getURL = "/api/get"
+    const baseURL = 'http://localhost:3001'
+    const getURL = '/api/get'
     // Send a GET request to the backend server to retrieve product data.
-    axios.get(baseURL+getURL).then((response) => {
+    axios.get(baseURL + getURL).then((response) => {
       setProductsList(response.data)
     })
   }, [])
-  
 
   // Current date in MySQL date format: YYYY-MM-DD
-  function getCurrentDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = ('0' + (today.getMonth() + 1)).slice(-2); // add leading zero if necessary
-    const day = ('0' + today.getDate()).slice(-2); // add leading zero if necessary
-    return `${year}-${month}-${day}`;
+  function getCurrentDate () {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = ('0' + (today.getMonth() + 1)).slice(-2) // add leading zero if necessary
+    const day = ('0' + today.getDate()).slice(-2) // add leading zero if necessary
+    return `${year}-${month}-${day}`
   }
 
   const addProduct = (event) => {
@@ -38,38 +36,38 @@ function App() {
     event.preventDefault()
 
     // Send a POST request to the backend server using axios, passing in the form data.
-    const baseURL = "http://localhost:3001"
-    const insertURL = "/api/insert"
+    const baseURL = 'http://localhost:3001'
+    const insertURL = '/api/insert'
     // Pass in the product name, description, price, creation date, and update date.
-    axios.post(baseURL+insertURL, {
-      productName: productName,
-      productDescription: productDescription,
-      productPrice: productPrice,
+    axios.post(baseURL + insertURL, {
+      productName,
+      productDescription,
+      productPrice,
       creationDate: getCurrentDate(),
       updateDate: getCurrentDate()
     }).then(() => {
-      console.log("Product successfully inserted into the database.")
+      console.log('Product successfully inserted into the database.')
     }).catch(error => {
-      console.error("Error inserting product:", error)
+      console.error('Error inserting product:', error)
     })
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Insert Product</h1>
 
-      <div className="form__wrapper">
+      <div className='form__wrapper'>
         <form onSubmit={addProduct}>
-          <label htmlFor="product-name">Product Name:</label>
-          <input type="text" id="product-name" name="product-name" maxLength="200" onChange={(e) => {setProductName(e.target.value)}} required/>
+          <label htmlFor='product-name'>Product Name:</label>
+          <input type='text' id='product-name' name='product-name' maxLength='200' onChange={(e) => { setProductName(e.target.value) }} required />
 
-          <label htmlFor="product-description">Product Description:</label>
-          <textarea id="product-description" name="product-description" maxLength="500" onChange={(e) => {setProductDescription(e.target.value)}} ></textarea>
+          <label htmlFor='product-description'>Product Description:</label>
+          <textarea id='product-description' name='product-description' maxLength='500' onChange={(e) => { setProductDescription(e.target.value) }} />
 
-          <label htmlFor="price">Price:</label>
-          <input type="number" id="price" name="price" step="0.01" min="0" max="9999999.99" onChange={(e) => {setProductPrice(e.target.value)}} required/>
+          <label htmlFor='price'>Price:</label>
+          <input type='number' id='price' name='price' step='0.01' min='0' max='9999999.99' onChange={(e) => { setProductPrice(e.target.value) }} required />
 
-          <button type="submit">Submit</button>
+          <button type='submit'>Submit</button>
         </form>
       </div>
 
@@ -80,13 +78,13 @@ function App() {
             <p>Product description: {singleProduct.descricao}</p>
             <p>Price: {singleProduct.preco}</p>
             <p>Insert date: {singleProduct.data_criacao}</p>
-            <p>Update date: {singleProduct.data_atualizacao}</p> <br/>
+            <p>Update date: {singleProduct.data_atualizacao}</p> <br />
           </div>
         )
       })}
 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 // Import dependencies
-const express = require("express")
+const express = require('express')
 const app = express()
-const mysql = require("mysql")
-const bodyParser = require("body-parser")
-const cors = require("cors")
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Creates a MySQL connection pool using the mysql library.
 //
@@ -14,11 +14,11 @@ const cors = require("cors")
 //   - password: The password to use for the authentication.
 //   - database: The name of the database to use.
 const db = mysql.createPool({
-  host: "127.0.0.1",
+  host: '127.0.0.1',
   port: '33061',
-  user: "root",
-  password: "ultra_secure_password",
-  database: "ChallengeDB",
+  user: 'root',
+  password: 'ultra_secure_password',
+  database: 'ChallengeDB'
 })
 
 // CORS module to enable communication between the front-end and back-end servers
@@ -26,25 +26,24 @@ app.use(cors())
 // grab the information as a JSON object from the front-end
 app.use(express.json())
 // Body Parser
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Handle an HTTP GET request to extract information from the database
 // req: represents the request data received from the front-end
 // res: represents the response data to be sent back to the front-end
-app.get("/api/get", (req, res) => {
+app.get('/api/get', (req, res) => {
   // Select all information from the store_products SQL table
-  const sqlSelectStatement = "SELECT * FROM store_products;"
+  const sqlSelectStatement = 'SELECT * FROM store_products;'
   // Execute the SELECT statement to return all information from the store_products SQL table
   db.query(sqlSelectStatement, (err, result) => {
     res.send(result)
   })
 })
 
-
 // Handle an HTTP POST request to insert information into the database
 // req: represents the request data received from the front-end
 // res: represents the response data to be sent back to the front-end
-app.post("/api/insert", (req, res) => {
+app.post('/api/insert', (req, res) => {
   // Extract the fields from the request body sent by the front-end
   const productName = req.body.productName // limit: 200 chars
   const productDescription = req.body.productDescription // limit: 500 chars
@@ -53,7 +52,7 @@ app.post("/api/insert", (req, res) => {
   const updateDate = req.body.updateDate // should be in YYYY-MM-DD format
 
   // Prepare an SQL INSERT statement with placeholders for the fields
-  const sqlInsertStatement = "INSERT INTO store_products (nome, descricao, preco, data_criacao, data_atualizacao) VALUES (?, ?, ?, ?, ?);"
+  const sqlInsertStatement = 'INSERT INTO store_products (nome, descricao, preco, data_criacao, data_atualizacao) VALUES (?, ?, ?, ?, ?);'
   // Execute the INSERT statement with the extracted field values
   db.query(sqlInsertStatement, [productName, productDescription, productPrice, creationDate, updateDate], (err, result) => {
     // Handle any errors that occurred during the query execution
@@ -61,6 +60,6 @@ app.post("/api/insert", (req, res) => {
   })
 })
 
-app.listen(3001, ()=>{
-  console.log("Server running on port 3001.")
+app.listen(3001, () => {
+  console.log('Server running on port 3001.')
 })
