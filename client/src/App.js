2cbcf5f22eb36b8eebe7e06d959f6968d081b33c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-28 17:06:39 -0300

    add the boilerplate for the POST request to insert a product
    Boilerplate for the function to add a new product to the database.
    - declare the React hooks for the name, the description and the price of the product
    - create the boilerplate for the function that will make the POST request to the back-end
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
-import './App.css';
+import React, { useEffect, useState } from "react"
+import Axios from "axios"
+
+// Stylesheet
+import "./App.css"
 
 function App() {
+
+  // Hooks for the product information
+  const [productName, setProductName] = useState("")
+  const [productDescription, setProductDescription] = useState("")
+  const [productPrice, setProductPrice] = useState(0)
+
+
+  const addProduct = (event) => {
+    // prevent the form from reloading the page when it's submitted
+    event.preventDefault()
+    // POST request from Axios - sending information into the backend
+    
+  }
+
   return (
     <div className="App">
       <h1>Insert Product</h1>
 
       <div className="form__wrapper">
-        <form>
-          <label for="product-name">Product Name:</label>
-          <input type="text" id="product-name" name="product-name" maxLength="200" required/>
+        <form onSubmit={addProduct}>
+          <label htmlFor="product-name">Product Name:</label>
+          <input type="text" id="product-name" name="product-name" maxLength="200" onChange={(e) => {setProductName(e.target.value)}} required/>
 
-          <label for="product-description">Product Description:</label>
-          <textarea id="product-description" name="product-description" maxLength="500" required></textarea>
+          <label htmlFor="product-description">Product Description:</label>
+          <textarea id="product-description" name="product-description" maxLength="500" onChange={(e) => {setProductDescription(e.target.value)}} ></textarea>
 
-          <label for="price">Price:</label>
-          <input type="number" id="price" name="price" step="0.01" min="0" max="9999999.99" required/>
+          <label htmlFor="price">Price:</label>
+          <input type="number" id="price" name="price" step="0.01" min="0" max="9999999.99" onChange={(e) => {setProductPrice(e.target.value)}} required/>
 
           <button type="submit">Submit</button>
         </form>
