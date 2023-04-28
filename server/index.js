cc5f20fd3ae5fd0c6cb841860fe557028cf69a2c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-28 14:19:02 -0300

    improve comment on creating a MySQL connection
    Add a more descriptive comment about the lines of code that creates a MySQL connection pool using the mysql.createPool() method provided by the mysql library.
    
    Once the pool is created, it can be used to handle multiple simultaneous connections to the database server.
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+// Import dependencies
 const express = require("express")
 const app = express()
 const mysql = require("mysql")
 
-// call the database
+// Creates a MySQL connection pool using the mysql library.
+//
+// The pool is configured with the following settings:
+//   - host: The hostname of the database server.
+//   - port: The port number to use for the database connection.
+//   - user: The username to authenticate with the database server.
+//   - password: The password to use for the authentication.
+//   - database: The name of the database to use.
 const db = mysql.createPool({
   host: "127.0.0.1",
   port: '33061',
