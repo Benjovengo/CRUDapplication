687cc0dad82aa1bc489bac4b6c149152f900c54d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-04-28 19:38:50 -0300

    add an HTTP POST request to insert information into the database
    - Extract the fields from the request body sent by the front-end
    - Prepare an SQL INSERT statement with placeholders for the fields
    - Execute the INSERT statement with the extracted field values
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- server/index.js
+++ server/index.js
@@ -28,6 +28,23 @@ app.use(express.json())
 // Body Parser
 app.use(bodyParser.urlencoded({extended: true}))
 
+// Handle an HTTP POST request to insert information into the database
+// req: represents the request data received from the front-end
+// res: represents the response data to be sent back to the front-end
+app.post("/api/insert", (req, res) =>{
+  // Extract the fields from the request body sent by the front-end
+  const productName = req.body.productName // limit: 200 chars
+  const productDescription = req.body.productDescription // limit: 500 chars
+  const productPrice = req.body.productPrice // limit: decimal(10,2)
+  const creationDate = req.body.creationDate // should be in YYYY-MM-DD format
+  const updateDate = req.body.updateDate // should be in YYYY-MM-DD format
+
+  // Prepare an SQL INSERT statement with placeholders for the fields
+  const sqlInsertStatement = "INSERT INTO store_products (nome, descricao, preco, data_criacao, data_atualizacao) VALUES (?, ?, ?, ?, ?);"
+  // Execute the INSERT statement with the extracted field values
+  db.query(sqlInsertStatement, [productName, productDescription, productPrice, creationDate, updateDate], (err, result) => {
+    // Handle any errors that occurred during the query execution
+    console.log(err)
   })
 })
 
