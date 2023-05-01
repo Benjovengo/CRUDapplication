15de8a0b44cd23c0f73479d15cffb84368b2a446
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-05-01 06:28:26 -0300

    add function to update the total of the purchase
    Create a check list with all the product items in the database.
    Increment the total amount of the purchase by selecting an item.
    - Unselect the item to subtract its value from the total amount of the purchase.
    
    Signed-off-by: Fabio Benjovengo <fabio.benjovengo@gmail.com>

--- client/src/pages/Shopping.jsx
+++ client/src/pages/Shopping.jsx
@@ -7,10 +7,11 @@ import './Stylesheet.css'
 
 const Shopping = () => {
   // Hooks for the sales information
-  const [totalPrice, setTotalPrice] = useState(0)
+  const [totalAmount, setTotalAmount] = useState(0)
   const [paymentType, setPaymentType] = useState('')
   const [status, setStatus] = useState('')
   const [salesList, setSalesList] = useState([])
+  const [productsList, setProductsList] = useState([])
 
   // Fetch the sales data from the database when the page loads or the sales
   // list changes
@@ -23,8 +24,69 @@ const Shopping = () => {
     })
   }, [salesList])
 
+  // Fetch the products from the database when the page loads
+  useEffect(() => {
+    // Address of the GET endpoint on the backend server
+    const getURL = 'http://localhost:3001/api/get'
+    // Send a GET request to the backend server to retrieve product data.
+    axios.get(getURL).then((response) => {
+      setProductsList(response.data)
+    })
+  }, [])
+
+  // Create the checkbox list when the products list is updated
+  useEffect(() => {
+    createProductsCheckboxes()
+  }, [productsList])
+
+  // Create the checkbox list when the products list is updated
+  useEffect(() => {
+    document.getElementById('total-checked').innerText = totalAmount
+  }, [totalAmount])
+  
+
+  // Generate a set of checkboxes in an HTML form, one for each object for the
+  // products array fetched from the database.
+  //
+  // When a user selects a checkbox, an alert box is displayed with the
+  // respective product name and price.
+  const createProductsCheckboxes = () => {
+    // Get a reference to the element with ID "products"
+    const productsDiv = document.getElementById('products');
+    productsList.forEach(product => {
+      // Create a new checkbox element
+      const checkbox = document.createElement('input')
+      // Set the type and name of the checkbox
+      checkbox.type = 'checkbox'
+      checkbox.name = 'product'
+      // Set the value of the checkbox to the product price
+      checkbox.value = product.preco
+
+      // Attach an event listener to the checkbox that displays an alert when it is selected
+      checkbox.addEventListener('change', () => {
+        const amount = document.getElementById('total-checked').innerText
+        if (checkbox.checked) {
+          // push item ID to an array
+          // add the value of the item to the total amount
+          setTotalAmount(Number(amount) + product.preco)
+        } else {
+          // pop the item ID from the array
+          // subtract the value of the item from the total amount
+          setTotalAmount(Number(amount) - product.preco)
+        }
+      })
+      // Create the label of the checkbox based on the product name
+      const label = document.createElement('label')
+      label.innerHTML = product.nome
+      // Append the checkbox, label, and a line break to the checkbox list
+      productsDiv.appendChild(checkbox)
+      productsDiv.appendChild(label)
+      productsDiv.appendChild(document.createElement('br'))
+    })
+  }
+
   // Current date in MySQL date format: YYYY-MM-DD
-  function getCurrentDate () {
+  const getCurrentDate = () => {
     const today = new Date()
     const year = today.getFullYear()
     const month = ('0' + (today.getMonth() + 1)).slice(-2) // add leading zero
@@ -45,7 +107,7 @@ const Shopping = () => {
     // Pass in the product name, description, price, creation date, and update
     // date.
     axios.post(registerSaleURL, {
-      totalPrice,
+      totalAmount,
       creationDate: getCurrentDate(),
       paymentType,
       status
@@ -62,8 +124,16 @@ const Shopping = () => {
       <h1>Shopping History</h1>
       <div className='form__wrapper'>
         <form onSubmit={registerSale}>
+
+          <div id='total-checked'></div>
+
+          <fieldset>
+            <legend>Select products:</legend>
+            <div id="products"></div>
+          </fieldset>
+
           <label htmlFor='total-amount'>Total amount:</label>
-          <input type='number' id='total-amount' name='total-amount' step='0.01' min='0' max='9999999.99' onChange={(e) => { setTotalPrice(e.target.value) }} required />
+          {/* <input type='number' id='total-amount' name='total-amount' step='0.01' min='0' max='9999999.99' onChange={(e) => { setTotalPrice(e.target.value) }} required /> */}
           <label htmlFor='payment-type'>Payment type:</label>
           <input type='text' id='payment-type' name='payment-type' maxLength='200' onChange={(e) => { setPaymentType(e.target.value) }} required />
           <label htmlFor='status'>Status:</label>
